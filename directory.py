3b85935b5efe565c78f89a7394cd363508d6ae33
JPabloA <94808157+JPabloA@users.noreply.github.com>
2024-05-31 16:01:50 -0600

    Directory and file classes

---
+++ directory.py
@@ -0,0 +1,47 @@
+import datetime
+from file import File
+
+
+class Directory:
+    def __innit__(self, name):
+        self.name = name
+        self.creationDate = datetime.now()
+        self.files = {}
+        self.directories = {}
+
+# To create a file in the directory
+def createFile (self, fileName, fileExtension, fileContent):
+    if fileName in self.files or fileName in self.directories:
+        raise ValueError("A file or directory with this name already exists.")
+    newFile = File(fileName, fileExtension, fileContent)
+    self.files[fileName] = newFile
+
+def createDirectory (self, directoryName):
+    if directoryName in self.files or directoryName in self.directories:
+        raise ValueError("A file or directory with this name already exists.")
+    newDirectory = Directory(directoryName)
+    self.directories[directoryName] = newDirectory;
+    
+def listDirectory(self):
+    contentList = []
+    for dirName in self.directories:
+        contentList.append(f"[DIR] {dirName}")
+    for fileName in self.files:
+        contentList.append(f"[FILE] {fileName}")
+    return contentList
+
+def remove_file(self, name):
+    if name in self.files:
+        del self.files[name]
+    else:
+        raise ValueError("File not found.")
+
+def remove_directory(self, name):
+    if name in self.directories:
+        if self.directories[name].directories or self.directories[name].files:
+            raise ValueError("Directory is not empty.")
+        del self.directories[name]
+    else:
+        raise ValueError("Directory not found.")
+    
+# TODO: Move and Find element
---
+++ file.py
@@ -0,0 +1,48 @@
+import datetime
+
+
+class File:
+    def __innit__(self, name, extension, content):
+        self.name = name
+        self.extension = extension
+        self.content = content
+        self.creationDate = datetime.now()
+        self.modificationDate = datetime.now()
+        self.size = len(content)
+        self.sectors = []
+    
+# Functions    
+
+# To modify the file content
+def modifyContent(self, newContent):
+    self.content = newContent
+    self.modificationDate = datetime.now()
+    self.size = len(newContent)
+
+# To view the file content 
+def viewContent(self):
+    # TODO: Que deberiamos de hacer aca?
+    return self.content
+
+# To view the file properties
+def viewProperties(self):
+    return {
+        "name": self.name,
+        "extension": self.extension,
+        "creationDate": self.creationDate,
+        "modificationDate": self.modificationDate,
+        "size": self.size
+        }
+
+# To assign file sectors
+def assignSectors(self, indicesSector):
+    self.sectors.extend(indicesSector)
+
+# To release file sectors
+# TODO: falta limpiar bien la estructura que vayamos a utilizar para el disco
+def releaseSectors(self):
+    sectors = self.sectors
+    self.sectors = []
+    return sectors
+    
+    
