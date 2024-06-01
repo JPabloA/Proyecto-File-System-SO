547a39c696f6312800d9e06be9160eb2003a3a1f
DeynerNB <deynernavarrob@gmail.com>
2024-06-01 17:57:00 -0600

    Load tree toplevel

---
+++ GUI/window_ShowTree.py
@@ -0,0 +1,32 @@
+from tkinter import Tk, Canvas, Entry, Text, Button, Toplevel
+
+class Window_ShowTree(Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.title("Display directory tree")
+        self.geometry("750x415")
+        self.configure(bg = "#FFFFFF")
+
+        canvas = Canvas(
+            self,
+            bg = "#FFFFFF",
+            height = 415,
+            width = 750,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+        canvas.place(x = 0, y = 0)
+
+        # Text input: Directory path
+        entry_1 = Entry( self, bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0 )
+        entry_1.place( x=9.0, y=10.0, width=722.0, height=33.0 )
+
+        # Button: Go back
+        button_1 = Button( self, text="Volver", borderwidth=0, highlightthickness=0, command=self.destroy, relief="flat" )
+        button_1.place( x=9.0, y=67.0, width=149.0, height=49.0 )
+
+        # Text Area: Display
+        entry_2 = Text( self, bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0 )
+        entry_2.place( x=171.0, y=67.0, width=558.0, height=323.0 )
